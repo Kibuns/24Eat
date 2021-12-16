4b3d1e9081877a93a18abcdf9737640d2f66507f
Jovi Simons <jovisimons009@gmail.com>
2021-12-16 23:38:35 +0100

    added urls and improved fetch categories

--- src/ApiService/ProductAPIService.js
+++ src/ApiService/ProductAPIService.js
@@ -1,4 +1,4 @@
-const PRODUCTS_REST_API = 'http://localhost:8080/categories';
+const PRODUCTS_REST_API = 'https://db01-4-menuservice.herokuapp.com/api/public/categories/';
 const CATEGORIESBYID_REST_API = 'http://localhost:8080/products/category/';
 const PRODUCTSBYID_REST_API = 'http://localhost:8080/products/';
 
@@ -18,14 +18,7 @@ class ProductAPIService {
     }
 
     getCategories(){
-        return fetch(PRODUCTS_REST_API,{ 
-            method: 'get',
-                headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                },
-                'credentials': 'same-origin'
-        })
+        return fetch(PRODUCTS_REST_API)
         .then(res => res.json());        
     }
 
